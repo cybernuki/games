33aaa03bd099e2377833e32442a7c279530a79d8
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-11 00:29:59 -0500

    dsa

--- x_win.c
+++ x_win.c
@@ -1,66 +1,68 @@
-#include <X11/Xlib.h>
+#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "libs_eng.h"
 
-void rend(Display *dis, Window window, int screen);
-void scanline(polygon **polys, vert_t **list, Display *dis, Window window, int screen);
+void rend(SDL_Window *window, SDL_Renderer *renderer);
+void scanline(polygon **polys, vert_t **list, SDL_Window *window, SDL_Renderer *renderer);
 void print_v(vert_t *ver, int line);
 
 scene *scn;
 
 int main(void)
 {
-	Display *dis;
-	Window window;
-	XEvent event;
-	scn = init_engine();
-	int screen;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Event event;
 
-	dis = XOpenDisplay(NULL);
-	if (dis == NULL)
+
+	scn = init_engine();
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
 	{
-		printf("\033[31mError opening display\n\033[0m");
+		printf("Error\n");
 		exit(1);
 	}
-	screen = DefaultScreen(dis);
-	window = XCreateSimpleWindow(dis, RootWindow(dis, screen), 0, 0,
-				     600, 600, 1, BlackPixel(dis, screen),
-				     WhitePixel(dis, screen));
-	XSelectInput(dis, window, ExposureMask | KeyPressMask);
-	XMapWindow(dis, window);
-	xrend_t *render = malloc(sizeof(xrend_t));
-	render->dis = dis;
-	render->window = window;
-	render-> screen = screen;
-	while (1)
+	window = SDL_CreateWindow("Games", 10, 10, 600, 600, SDL_WINDOW_SHOWN);
+	if (window == NULL)
 	{
-		XNextEvent(dis, &event);
-		if (event.type == Expose)
+		printf("pailas\n");
+		exit(1);
+	}
+	renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+	
+	printf("%s %d\n", __FILE__, __LINE__);
+/*	SDL_FillRect(sur, NULL, SDL_MapRGB(sur->format, 0xFF, 0xFF, 0xFF));
+	printf("%s %d\n", __FILE__, __LINE__);
+	SDL_UpdateWindowSurface(window);
+	printf("%s %d\n", __FILE__, __LINE__);*/
+	SDL_Delay(5000);
+/*	while (1)
+	{
+		SDL_PollEvent(&event);
+		if (event.type == SDL_QUIT)
+			break;
+		if (event.type == SDL_KEYDOWN)
 		{
-			printf("%s %d:  redrawing\n", __FILE__,__LINE__);
-			rend(dis, window, screen);
-		}
-		if (event.type == KeyPress)
-		{
-		    if (key_pressed(event.xkey.keycode) == 1)
-		    {
-			    rend(dis, window, screen);
-		    }
-		    if (event.xkey.keycode == 0x09)
+
+		    rend(window, renderer);
+		    if (event.key.keysym.sym == SDLK_ESCAPE)
 			break;
 		}
-	}
-	XCloseDisplay(dis);
+		}*/
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	(void) event;
+	
+	(void) renderer;
 	return (0);
 }
 
-void rend(Display *dis, Window window, int screen)
+void rend(SDL_Window *window, SDL_Renderer *renderer)
 {
 	vert_t *vrtx;
-	XClearWindow(dis, window);
-	XSetForeground(dis, DefaultGC(dis, screen), 0x00000000);
+
 	*(scn->camara->target) = get_target_origin(scn->obj);
 	vrtx = draw_vertex(&(scn->obj), scn->camara,
 			   scn->viewp);
@@ -68,19 +70,13 @@ void rend(Display *dis, Window window, int screen)
 	vert_t *vt = *(&(vrtx));
 	while (vt != NULL)
 	{
-/*		printf("vert: %c\n", pos);*/
-/*		XFillRectangle(dis, window,
-		DefaultGC(dis, screen), vt->x, vt->y, 4 , 4);*/
-/*		XDrawString(dis, window, DefaultGC(dis, screen),
-			    vt->x, vt->y,
-			    (char *) &pos, 1);*/
 		vt = vt->next;
 		pos++;
 
 	}
-/*	print_polygons(&(scn->obj->polys));*/
-	XSetForeground(dis, DefaultGC(dis, screen), 0x00000000);
-	scanline(&(scn->obj->polys), &vrtx, dis,window, screen);
+
+
+	scanline(&(scn->obj->polys), &vrtx, window, renderer);
 
 }
 
@@ -108,19 +104,16 @@ void print_v(vert_t *ver, int line)
 	printf("%s %d:  x: %.2f y: %.2f z: %.2f\n",__FILE__, line, ver->x, ver->y, ver->z);
 }
 
-void scanline(polygon **polys, vert_t **list, Display *dis, Window window, int screen)
+void scanline(polygon **polys, vert_t **list, SDL_Window *window, SDL_Renderer *renderer)
 {
+	(void) window;
+	(void) renderer;
 	printf("init scanline\n");
 	polygon *ps = *polys;
 	vert_t *vrtx = *list;
 	vert_t *arr[3];
 	int position = 1;
 
-	const char *font = "*schumacher*-16-*";
-	XFontStruct *f = XLoadQueryFont(dis, font);
-	if (! f )
-		f = XLoadQueryFont(dis, "fixed");
-	XSetFont(dis, DefaultGC(dis, screen), f->fid);
 	while (ps != NULL)
 	{
 		vrtx = *list;
@@ -158,15 +151,6 @@ void scanline(polygon **polys, vert_t **list, Display *dis, Window window, int s
 
 		if ((position) != 0)
 		{
-/*		XDrawString(dis, window, DefaultGC(dis, screen),
-			    arr[0]->x + 4, arr[0]->y - 4,
-			    "0", 1);
-		XDrawString(dis, window, DefaultGC(dis, screen),
-			    arr[1]->x + 4, arr[1]->y - 4,
-			    "1", 1);
-		XDrawString(dis, window, DefaultGC(dis, screen),
-			    arr[2]->x + 4, arr[2]->y - 4,
-			    "2", 1);*/
 		for (int y = 0; y < (arr[2]->y - arr[0]->y); y++)
 		{
 
@@ -176,11 +160,6 @@ void scanline(polygon **polys, vert_t **list, Display *dis, Window window, int s
 			printf("%.2f %.2f %.2f\n", arr[0]->x, arr[1]->x, arr[2]->x);
 			r = ((y * X) / Y) + arr[0]->x;
 			printf("result x %.2f  ", r);
-			XSetForeground(dis, DefaultGC(dis, screen), 0x00fb1010);
-			/*	XFillRectangle(dis, window,
-				   DefaultGC(dis, screen),
-				       r , y + arr[0]->y,
-				       4, 4);*/
 			float tmpX, tmpY, tmpR = r;
 			if ((y + arr[0]->y) >= arr[1]->y)
 			{
@@ -190,7 +169,6 @@ void scanline(polygon **polys, vert_t **list, Display *dis, Window window, int s
 				Y = arr[2]->y - arr[1]->y;
 				tmpX = arr[1]->x;
 				tmpY = y - (arr[1]->y - arr[0]->y);
-				XSetForeground(dis, DefaultGC(dis, screen), 0x0034acfb);
 			}
 			else
 			{
@@ -198,41 +176,19 @@ void scanline(polygon **polys, vert_t **list, Display *dis, Window window, int s
 				Y = arr[1]->y - arr[0]->y;
 				tmpX = arr[0]->x;
 				tmpY = y;
-				XSetForeground(dis, DefaultGC(dis, screen), 0x00fb10ac);
 
 			}
 			r = ((tmpY * X) / Y);
 			r += tmpX;
 			printf("result x %.2f tmpy: %.2f\n", r, tmpY);
+			(void) tmpR;
 
 
-/*			XFillRectangle(dis, window,
-				   DefaultGC(dis, screen),
-				       r , y + arr[0]->y,
-				       4, 4);*/
 
-
-			XDrawLine(dis, window, DefaultGC(dis, screen),
-			  (int) tmpR, (int) y + arr[0]->y,
-			  (int) r, (int) y + arr[0]->y);
 
 		}
 
 		}
-
-				XSetForeground(dis, DefaultGC(dis, screen), 0x00000000);
-	      	XDrawLine(dis, window, DefaultGC(dis, screen),
-			  (int) arr[0]->x, (int) arr[0]->y,
-			  (int) arr[1]->x, (int) arr[1]->y);
-
-
-		XDrawLine(dis, window, DefaultGC(dis, screen),
-			  (int) arr[1]->x, (int) arr[1]->y,
-			  (int) arr[2]->x, (int) arr[2]->y);
-
-		XDrawLine(dis, window, DefaultGC(dis, screen),
-			  (int) arr[2]->x, (int) arr[2]->y,
-			  (int) arr[0]->x, (int) arr[0]->y);
 
 		print_v(arr[0], __LINE__);
 		print_v(arr[1], __LINE__);
